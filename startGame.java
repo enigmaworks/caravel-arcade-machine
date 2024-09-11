96aed7f5ceb939d9bce378ee2470b79f8dd44f99
Eddie Radecki <eddie.radecki@gmail.com>
2024-09-11 19:38:46 -0400

    setup files

---
+++ startGame.java
@@ -0,0 +1,5 @@
+public class startGame {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ timer.java
@@ -0,0 +1,5 @@
+public class timer {
+    public static void main(String[] args){
+        
+    }
+}
