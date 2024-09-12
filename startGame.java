a208d1d07423ede667418206c6a075e5818801a1
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2024-09-12 11:02:06 -0400

    better formatting

--- startGame.java
+++ startGame.java
@@ -7,27 +7,26 @@ import java.io.FileNotFoundException;
 
 public class startGame {
     public static void main(String[] args){      
-        File pauseFile = new File("pause.txt");
-        File timeFile = new File("time.txt");
+        File pauseFile = new File("pause.txt"); //contains a single boolean value
+        File timeFile = new File("time.txt"); //contains a single int 0 or higher
 
         try {
+            // read current time 
             Scanner timeFileReader = new Scanner(timeFile);
             int time = 0;
-
             if(timeFileReader.hasNextLine()){
                 time = Integer.parseInt(timeFileReader.nextLine());
             }
-
             timeFileReader.close();
             System.out.println(time);
             
-            if(time <= 0){
-                
-            } else {
+            Boolean pause = false;
+            
+            if(time > 0) {
+                //time remains to play. Open game.
+
                 // retroarch --fullscreen -L /path/to/core.so /path/to/rom
                 // Runtime.getRuntime().exec("retroarch --fullscreen -L " + args[0] + " " + args[1]);
-                
-                Boolean pause = false;
 
                 while(time > 0 && pause == false){
                     try {
@@ -58,19 +57,24 @@ public class startGame {
                         System.out.println(e);
                     }
 
-                    if(pause == false){
-                        //show coin prompt
-                    } else {
-                        try {
-                            FileWriter writer = new FileWriter(pauseFile);
-                            writer.write("false");
-                            writer.close();
-                        } catch (IOException e){
-                            System.out.println(e);
-                        }
-                    }
                 }
             }
+
+            if(pause == false){
+                // no time left to play:
+                // simulate pause key press
+                // show coin prompt
+            } else {
+                // player exited the game, reset varaibles and end script
+                try {
+                    FileWriter writer = new FileWriter(pauseFile);
+                    writer.write("false");
+                    writer.close();
+                } catch (IOException e){
+                    System.out.println(e);
+                }
+            }
+
         } catch (FileNotFoundException e){ System.out.println(e); }
     }
 }
